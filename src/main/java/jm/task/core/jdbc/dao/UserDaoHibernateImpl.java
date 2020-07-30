0f9155b88a4c884cd4717ae4a065488c33c5db91
Vyacheslav-Ko <Ignition1978@gmail.com>
2020-07-30 18:18:51 +0500

    final

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -2,7 +2,6 @@ package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
 import jm.task.core.jdbc.util.Util;
-import org.hibernate.Hibernate;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 import org.hibernate.query.Query;
@@ -16,28 +15,12 @@ public class UserDaoHibernateImpl implements UserDao {
 
     @Override
     public void createUsersTable() {
-        /*String SQL_CREATE = "CREATE TABLE IF NOT EXISTS USERS (" +
-                "id INT(10) NOT NULL AUTO_INCREMENT," +
-                "name VARCHAR(45) NOT NULL," +
-                "lastName VARCHAR(45) NOT NULL," +
-                "age INT (3) NOT NULL, PRIMARY KEY (id))";
-        Session session = Util.getSessionFactory().openSession();
-        Transaction transaction = session.beginTransaction();
-        session.createSQLQuery(SQL_CREATE);
-        transaction.commit();
-        System.out.println("++++++++++++++++++ CREATE +++++++++++++++++++++++++++");
-        session.close();*/
+
     }
 
     @Override
     public void dropUsersTable() {
-        /*String SQL_DROP = "DROP TABLE IF EXISTS USERS";
-        Session session = Util.getSessionFactory().openSession();
-        Transaction transaction = session.beginTransaction();
-        session.createSQLQuery(SQL_DROP);
-        transaction.commit();
-        System.out.println("++++++++++++++++++ DROP +++++++++++++++++++++++++++");
-        session.close();*/
+
     }
 
     @Override
@@ -48,7 +31,6 @@ public class UserDaoHibernateImpl implements UserDao {
         session.save(user);
         transaction.commit();
         session.close();
-        //rollback ????
     }
 
     @Override
@@ -66,7 +48,6 @@ public class UserDaoHibernateImpl implements UserDao {
             Session session = Util.getSessionFactory().openSession();
             Transaction transaction = session.beginTransaction();
             List<User> userList = session.createQuery("SELECT a FROM User a", User.class).getResultList();
-            //List<User> userList = (List<User>) session.createSQLQuery("SELECT * FROM users").list();
             transaction.commit();
             session.close();
             return userList;
